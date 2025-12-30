004b3b7095e09bab3075b7ceacef7892927b8a5a
Massad Raza <massad.raza@hotmail.com>
2025-12-30 14:16:30 -0500

    Adding function to subset dataset

--- datasetConverter.py
+++ datasetConverter.py
@@ -2,15 +2,24 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn.model_selection import train_test_split
+def shoulderSeasonsOnly():
 
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.tree import plot_tree, export_graphviz
+    df = pd.read_csv("Smith-Kerns Dataset 2026.xls")
 
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.pipeline import Pipeline
-from sklearn.ensemble import GradientBoostingRegressor
+    numeric_cols = [
+        'Foci', 'meanLW', 'meanST', 'meanSM', 'meanRH', 'meanAT', 'meanRF', 
+        'maxLW', 'maxST', 'maxSM', 'maxRH', 'maxAT', 'maxRF', 'minST', 'minSM',
+        'minRH', 'minAT'
+    ]
 
- 
+    for col in numeric_cols:
+        df[col] = df[col].replace('.', np.nan).astype(float)
+    
+    # Drop columns that are missing Foci values
+    df_cleaned = df.dropna(subset=['Foci']).copy()
+
+    SPRING_SHOULDER_END = 165 # Up to and including June 14th
+    FALL_SHOULDER_START = 259 # From September 16th (including the 16th)
+
+    spring_shoulder = df_cleaned['DayOfYear'] <= SPRING_SHOULDER_END
+    fall_shoulder = df_cleaned['DayOfYear'] >= FALL_SHOULDER_START
